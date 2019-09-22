509062a1c70a43e375aa2a65e5c81d3d69ba2e74
Chengsi <gaochengsi@gmail.com>
2019-09-22 12:04:37 -0700

    add scanner projects

--- src/com/helloworld/Main.java
+++ src/com/helloworld/Main.java
@@ -1,8 +1,13 @@
 package com.helloworld;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
 import java.awt.*;
+import java.text.FieldPosition;
+import java.text.NumberFormat;
+import java.text.ParsePosition;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.Scanner;
 
 public class Main {
 
@@ -73,13 +78,66 @@ public class Main {
 
         int v = 1;
         v++; // same as post fix ++v
-        int y = v++; // x will first be copied to y, then x will be incremented to 2
+        int z = v++; // x will first be copied to y, then x will be incremented to 2
         // print x, y will be 2, 1
         // but if prefix y = ++x;
         //print x, y will be 2, 2
-        System.out.println(v);
+        System.out.println(z);
 
+        // implicit casting and type conversion - happens when data will not be lost
+        short s = 1;
+        int r = s + 2;
+        System.out.println(r); //3. short byte: 2, int byte 4.
+        // java implicit casting (automatic conversion) byte > short > int > long
+        double dou = 1.1;
+        double res = dou + 2; // 3.1: double + integer (less precise so will be auto casted to 2.0)
+        int get3 = (int) dou + 2; // 3
+        String str = "1";
+        get3 = Integer.parseInt(str) + 2; // Integer is the wrapper class
+        // if Integer.parseInt(float string "1.1") will get Exception error => Double.parseDouble("1.1")
 
+        Math.round(1.1F); // int 1. round returns a long not an int
+        //Math.round(); returns a double 0-1
+        int roundRandomRes = (int) Math.round(Math.random() * 100); // int 0-100
+
+        // abstract class can't create a new instance with the new instead use factory method below
+        NumberFormat currency = NumberFormat.getCurrencyInstance(); //.getPercentInstance()
+        String resCurrency = currency.format(12345.891); // string rep in currency
+        System.out.println(resCurrency); //$12,345.89
+        //method chaining
+        String perc = NumberFormat.getPercentInstance().format(0.1);
+        System.out.println(perc); // 10%
+
+//        Scanner scanner = new Scanner(System.in); // can type number in terminal and see result
+//        System.out.print("enter age: "); // can only enter integer
+        //byte ageScan = scanner.nextByte();
+        //System.out.println("You are " + ageScan); // implicit casting/conversion
+//        System.out.print("enter your name: ");
+//        String yourName = scanner.nextLine().trim();
+//        System.out.println("my name is " + yourName);
+
+        //Mortgage calculator
+        final byte MONTH_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Principal: "); // if enter non int will crash the program, use conditional logic later
+        int principal = scanner.nextInt();
+
+        System.out.print("Annual Interest Rate: ");
+        float annualInterest = scanner.nextFloat(); // interest rate is small enough don't need double
+        float monthlyInterest = annualInterest / PERCENT / MONTH_IN_YEAR;
+
+        System.out.print("Period (Years): ");
+        byte years = scanner.nextByte();
+        int numberOfPayments = years * MONTH_IN_YEAR;
+
+        double mortgage = principal * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+                                    / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+
+        String mortgageFormated = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: " + mortgageFormated);
     }
 
 }
